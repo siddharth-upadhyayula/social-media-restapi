825cf975b791a6b633dad81f0ec014a3aac37410
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-01-15 17:44:00 -0500

    Created the repository

---
+++ src/main/java/com/sidupadhyayula/socialmediarestapi/SocialMediaRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.sidupadhyayula.socialmediarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaRestapiApplication.class, args);
+	}
+
+}
