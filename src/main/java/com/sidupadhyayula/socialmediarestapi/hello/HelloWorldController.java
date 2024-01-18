cfa7ae009f7ccb6115bed149e8362f604218ba92
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-01-17 19:12:33 -0500

    Internationalize content

--- src/main/java/com/sidupadhyayula/socialmediarestapi/hello/HelloWorldController.java
+++ src/main/java/com/sidupadhyayula/socialmediarestapi/hello/HelloWorldController.java
@@ -1,11 +1,23 @@
 package com.sidupadhyayula.socialmediarestapi.hello;
 
+import java.util.Locale;
+
+import org.springframework.cglib.core.Local;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController (MessageSource messageSource) {
+		this.messageSource = messageSource;
+		
+	}
 	
 	@GetMapping(path = "/helloworld")
 	public String HelloWorld(){
@@ -20,5 +32,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/helloworldbean/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
 		return new HelloWorldBean("Hello World, " + name);
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String HelloWorldInternationalized(){
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 }
